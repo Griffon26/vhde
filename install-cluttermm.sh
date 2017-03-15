63214e1bada3dee835221e086d48354b44477c4a
ben <ben.spoor@gmail.com>
2017-03-15 23:03:06 +0100

    Install in HOME/external dir

--- install-cluttermm.sh
+++ install-cluttermm.sh
@@ -2,4 +2,8 @@
 set -ex
 wget https://github.com/GNOME/cluttermm/archive/master.zip -O /tmp/cluttermm.zip
 unzip /tmp/cluttermm.zip
-cd cluttermm-master && ./autogen.sh && ./configure --disable-documentation && make && make install
+mkdir -p $HOME/external
+cd cluttermm-master && \
+./autogen.sh && \
+./configure --prefix=$HOME/external --disable-documentation && \
+make && make install
