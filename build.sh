50e17601bd9505c7c9eecfcc110544912e66d52d
Patric Theune <patric.theune@gmail.com>
2017-07-13 23:45:57 +0200

    Removing compile vhde from build, for now

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ ACLOCAL_PATH="/home/patric/.local/share/aclocal"
 export ACLOCAL_PATH
 
 ./install-cluttermm.sh
+
 export INSTALLDIR=/home/patric/.local
 export PKG_CONFIG_PATH="${INSTALLDIR}/lib/pkgconfig:${INSTALLDIR}/share/pkgconfig"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${INSTALLDIR}/lib"
 ./install-clutter-gtkmm.sh
 
 cmake .
-make
+#make
